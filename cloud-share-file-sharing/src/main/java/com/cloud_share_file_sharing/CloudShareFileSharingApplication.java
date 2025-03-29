a61aefe82e8e7cf357fe7b163ec10d2a43ef79a4
naveen585 <snaveen585@gmail.com>
2025-03-29 00:13:59 -0500

    backend code setup.

---
+++ cloud-share-file-sharing/src/main/java/com/cloud_share_file_sharing/CloudShareFileSharingApplication.java
@@ -0,0 +1,13 @@
+package com.cloud_share_file_sharing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudShareFileSharingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudShareFileSharingApplication.class, args);
+	}
+
+}
---
+++ cloud-share-file-sharing/src/test/java/com/cloud_share_file_sharing/CloudShareFileSharingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloud_share_file_sharing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudShareFileSharingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
